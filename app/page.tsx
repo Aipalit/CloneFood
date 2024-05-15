841fbd1ad618278b5fcd0658eb042083587d6ab7
Aipalit <lucas.gomes.souza1997@gmail.com>
2024-05-14 21:28:48 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 const Home = () => {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <div className="px-5 pt-4">
+        <Search></Search>
+      </div>
+    </>
+  );
 };
 
 export default Home;
