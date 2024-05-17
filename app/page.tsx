48389912859b1077225c76aff4a07a225ef0a472
Aipalit <lucas.gomes.souza1997@gmail.com>
2024-05-17 20:31:16 -0300

    feat: add second promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -6,6 +5,7 @@ import ProductList from "./product-list";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
+import PromoBanner from "./_components/promo-banner";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -34,16 +34,13 @@ const Home = async () => {
         <CategoryList />
       </div>
 
-      <div className="px-5 pt-6"></div>
-      <Image
-        src="/promo-banner01.png"
-        alt="Até 30% de desconto em pizzas!"
-        height={0}
-        width={0}
-        className="h-auto w-full object-contain"
-        sizes="100vw"
-        quality={100}
-      />
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/promo-banner01.png"
+          alt="Até 30% de desconto em pizzas!"
+        />
+      </div>
+
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold"> Pedidos recomendados</h2>
@@ -56,6 +53,12 @@ const Home = async () => {
           </Button>
         </div>
         <ProductList products={products} />
+      </div>
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/promo-banner02.png"
+          alt="A partir de R$17,90 em lanches"
+        />
       </div>
     </>
   );
